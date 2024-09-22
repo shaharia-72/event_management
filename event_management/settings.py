83379098a1d0ea047da994c6cfa6a5103a8321d8
Shaharia <shahariahossain72@gmail.com>
2024-09-22 13:31:51 +0600

    add_CSRF

--- event_management/settings.py
+++ event_management/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # Your secret key
 SECRET_KEY = env("SECRET_KEY")
-...
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
+CSRF_TEUSTED_ORIGINS =['https://event-management-8d69.onrender.com', 'http://*.127.0.0.1/']
 
 
 # Application definition
